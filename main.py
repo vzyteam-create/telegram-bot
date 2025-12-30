588ab98c31a527ec7faf7aab3bcee5ce4c235814
vzyteam-create <vzyteam@gmail.com>
2025-12-30 23:23:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+import os
 
+bot = Bot(token=os.getenv("8514866233:AAG6SYtXYkfkB3k5FUtVFLZDMgsYKiPbYKY"))
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    await message.answer("Бот запущен ✅")
+
+if __name__ == "__main__":
+    executor.start_polling(dp)

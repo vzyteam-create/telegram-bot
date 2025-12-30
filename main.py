71881a6bbf2f8f7e45b4a0af8bfbf2bb637923ae
vzyteam-create <vzyteam@gmail.com>
2025-12-31 00:17:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
-import os
 
-bot = Bot(token=os.getenv("8406040455:AAErPgkbH183hkLttTTgye_9S7MyOJtwi1M"))
+TOKEN = os.getenv("8406040455:AAErPgkbH183hkLttTTgye_9S7MyOJtwi1M")
+
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=["start"])
-async def start(message: types.Message):
-    await message.answer("Бот запущен ✅")
+async def start_cmd(message: types.Message):
+    await message.answer("Бот работает ✅")
+
+async def on_startup(dp):
+    await bot.delete_webhook(drop_pending_updates=True)
+    print("Webhook удалён, бот запущен")
 
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    executor.start_polling(
+        dp,
+        skip_updates=True,
+        on_startup=on_startup
+    )
